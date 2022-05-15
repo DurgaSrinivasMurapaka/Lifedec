ff30ee500f238ebc2e4ce3f4f201be95dfd8d7a1
Sree <srinu.murapaka2000@gmail.com>
2022-05-15 22:02:31 +0530

    commit-background-only

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 //test command for commit
 function App() {
@@ -6,18 +5,11 @@ function App() {
     
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           <code>Life Decisions game will be here.</code>
         </p>
-        <a
-          className="App-link"
-          href="https://github.com/durgasrinivasmurapaka"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My Github link.
-        </a>
+        
       </header>
     </div>
   );
