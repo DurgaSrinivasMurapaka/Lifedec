b06a82f771f3db624244abd223b8e2fbdda0d744
Sree <srinu.murapaka2000@gmail.com>
2022-05-15 20:32:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Life Decisions game will be here.
+          <code>Life Decisions game will be here.</code>
         </p>
         <a
           className="App-link"
