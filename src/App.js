f6bb1a80835461603e03ebe102b8766acf2922a3
Sree <srinu.murapaka2000@gmail.com>
2022-05-15 21:49:20 +0530

    last react basic commit before edit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          My Github link
+          My Github link.
         </a>
       </header>
     </div>
