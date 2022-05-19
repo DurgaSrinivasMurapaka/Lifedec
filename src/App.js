9da539dba2a7b3b93c339c205ccc0bef8e0a0063
Sree <srinu.murapaka2000@gmail.com>
2022-05-19 19:32:43 +0530

    1st actual commit- 3 buttons added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,20 @@ import React from 'react'
     return (
       
       <div className="container">
-        <input type="button" value="Next Decision" onclick="msg()"/>
+
+
+
+        <div class="buttons">
+		      <button name="submit" class="action_btn Ledger" type="submit" value="Ledger" onclick="myFunction()">Ledger</button>
+          <div class="space"></div>
+          <button name="submit" class="action_btn Next" type="submit" value="Next" onclick="myFunction2()">Next Decision</button>
+          <div class="space"></div>
+		      <button name="submit" class="action_btn Stats" type="submit" value="Stats" onclick="myFunction2()">Stats</button>
+		    <p id="saved"></p>
+        </div>
+
+
+
       </div>
     )
   }
